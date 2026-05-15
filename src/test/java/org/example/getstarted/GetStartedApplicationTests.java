d01d36487860d3b5ed978c8f9bd832bd0c301989
Adil <adilturashev.@gmail.com>
2026-05-15 10:26:36 +0500

    feat: initial Spring Boot setup

---
+++ src/test/java/org/example/getstarted/GetStartedApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.getstarted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetStartedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
